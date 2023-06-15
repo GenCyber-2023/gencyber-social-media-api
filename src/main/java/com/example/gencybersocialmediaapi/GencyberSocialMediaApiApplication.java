d26b2c75822d8d35c99a3806969cf825c42f77aa
Domenic Mangano <dm9965@rit.edu>
2023-06-15 10:17:55 -0400

    committing beginning of gencyber social media ui

---
+++ src/main/java/com/example/gencybersocialmediaapi/GencyberSocialMediaApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.gencybersocialmediaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GencyberSocialMediaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GencyberSocialMediaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gencybersocialmediaapi/GencyberSocialMediaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gencybersocialmediaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GencyberSocialMediaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
